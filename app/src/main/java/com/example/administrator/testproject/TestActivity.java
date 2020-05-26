b7a9b755326efb50c66c9340ecc9568e9d01112b
chen-hs <468453790@qq.com>
2020-05-26 15:21:20 +0800

    Add jump event.

--- app/src/main/java/com/example/administrator/testproject/TestActivity.java
+++ app/src/main/java/com/example/administrator/testproject/TestActivity.java
@@ -1,13 +1,35 @@
 package com.example.administrator.testproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
+
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        initView();
+        initLinner();
     }
+
+    private void initView(){
+        mBtn = (Button)this.findViewById(R.id.btn);
+    }
+
+    private void initLinner(){
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
